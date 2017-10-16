816bd90594e304cbe2d2625705d5b0576babd00e
Gabriel Cardona <gabriel.cardona@bluerocket.us>
2017-10-16 15:52:17 -0700

    exchange rate.

--- src/services/blocktrail.service.ts
+++ src/services/blocktrail.service.ts
@@ -8,8 +8,8 @@ export class BlocktrailService {
   constructor() {
     this.client = Blocktrail.BlocktrailSDK({
       network: "BCC",
-      apiKey: "f4b4845078a7f583e28f387f00e6d954f5cac26a",
-      apiSecret: "da449cedb5bc2bd28f462a9a04fdf47b8e07e441",
+      apiKey: "",
+      apiSecret: "",
       testnet: false
     });
   }
@@ -43,5 +43,9 @@ export class BlocktrailService {
     return this.client.addressTransactions(address, {
       page: page
     },(err, block) => {});
+  }
+
+  getExchangeRate(): Promise<any>{
+    return this.client.price((err, block) => {});
   }
 }
